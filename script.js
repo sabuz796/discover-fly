180a5c6a4f9a5fc6dd8fe0523eb91e0c1ae76581
sabuz796 <sabuz796@gmail.com>
2021-06-22 18:38:40 +0600

    first-comit

---
+++ script.js
@@ -0,0 +1,17 @@
+let firstIncreaseButton = document.querySelector('#first-increase-button');
+let firstDecreaseButton = document.querySelector('#first-decrease-button');
+let firstInputBox = document.querySelector('#first-input-box');
+
+
+
+firstIncreaseButton.addEventListener('click', () => {
+
+    if (firstIncreaseButton.value = ' ') {
+        firstInputBox.value = 0;
+    }
+    
+    firstInputBox.value = parseFloat(firstInputBox.value) + 1
+    
+
+})
+
