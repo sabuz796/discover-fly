0f48ee7f15c3a43103fd55adc3016c2772ec1ac4
sabuz796 <sabuz796@gmail.com>
2021-06-23 07:30:22 +0600

    created function to avoid duplicate code

--- script.js
+++ script.js
@@ -2,19 +2,90 @@ let firstIncreaseButton = document.querySelector('#first-increase-button');
 let firstDecreaseButton = document.querySelector('#first-decrease-button');
 let firstInputBox = document.querySelector('#first-input-box');
 
+let economyIncreaseButton = document.querySelector('#economy-increase-button');
+let economyDecreaseButton = document.querySelector('#economy-decrease-button');
+let economyInputBox = document.querySelector('#economy-input-box');
+
+let subTotal = document.querySelector('#subtotal');
+let vat = document.querySelector('#vat');
+let total = document.querySelector('#total');
+
+let firstTicketPrice;
+let economyTicketPrice;
 
 
-firstIncreaseButton.addEventListener('click', () => {
+// set initial value of input boxes
+firstInputBox.value = 0;
+economyInputBox.value = 0;
 
-    if (firstIncreaseButton.value = ' ') {
-        firstInputBox.value = 0;
+
+function increaseDecrease(inputBox, ticketPrice, isIncrease){
+    if(isIncrease == true){
+        inputBox.value = parseInt(inputBox.value) + 1;
     }
+    else{
+        inputBox.value = parseInt(inputBox.value) - 1;
+    }  
+    ticketPrice = inputBox.value * 150;
+}
 
-    firstInputBox.value = parseFloat(firstInputBox.value) + 1
-
-
-    // test to show........
-    
-
+// first class increase button handler
+firstIncreaseButton.addEventListener('click', () => {
+    // firstInputBox.value = parseInt(firstInputBox.value) + 1;
+    // firstTicketPrice = firstInputBox.value * 150;
+    increaseDecrease(firstInputBox, firstTicketPrice, true);
+    // calculate(firstInputBox, firstTicketPrice);
+    // subTotal.innerText = parseFloat(firstTicketPrice) + parseFloat(economyTicketPrice);
+    subTotal.innerText = parseFloat(firstInputBox.value * 150) + parseFloat(economyInputBox.value * 100);
+    vat.innerText = parseInt(subTotal.innerText) * (10 / 100)
+    total.innerText = parseInt(subTotal.innerText) + parseInt(vat.innerText);
 })
+
+
+
+
+// first class decrease button handler
+firstDecreaseButton.addEventListener('click', () => {
+    // firstInputBox.value = parseInt(firstInputBox.value) - 1;
+    // firstTicketPrice = firstInputBox.value * 150;
+
+    increaseDecrease(firstInputBox, firstTicketPrice, false)
+    // subTotal.innerText = parseFloat(firstTicketPrice) + parseFloat(economyTicketPrice);
+    subTotal.innerText = parseFloat(firstInputBox.value * 150) + parseFloat(economyInputBox.value * 100);
+    vat.innerText = parseInt(subTotal.innerText) * (10 / 100)
+    total.innerText = parseInt(subTotal.innerText) + parseInt(vat.innerText);
+
+
+
+    console.log(firstTicketPrice);
+})
+
+// economy increase button handler
+economyIncreaseButton.addEventListener('click', () => {
+    economyInputBox.value = parseInt(economyInputBox.value) + 1;
+    economyTicketPrice = economyInputBox.value * 100;
+    // subTotal.innerText = parseFloat(firstTicketPrice) + parseFloat(economyTicketPrice);
+    subTotal.innerText = parseFloat(firstInputBox.value * 150) + parseFloat(economyInputBox.value * 100);
+    vat.innerText = parseInt(subTotal.innerText) * (10 / 100)
+    total.innerText = parseInt(subTotal.innerText) + parseInt(vat.innerText);
+
+
+    console.log(economyTicketPrice);
+})
+
+// economy decrease button handler
+economyDecreaseButton.addEventListener('click', () => {
+    economyInputBox.value = parseInt(economyInputBox.value) - 1;
+    economyTicketPrice = economyInputBox.value * 100;
+    // subTotal.innerText = parseFloat(firstTicketPrice) + parseFloat(economyTicketPrice);
+    subTotal.innerText = parseFloat(firstInputBox.value * 150) + parseFloat(economyInputBox.value * 100);
+    vat.innerText = parseInt(subTotal.innerText) * (10 / 100)
+    total.innerText = parseInt(subTotal.innerText) + parseInt(vat.innerText);
+
+
+
+    console.log(economyTicketPrice);
+})
+
+// created function to avoid duplicate code
 
